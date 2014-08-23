3d02fabccb379409c339cd683b8ec802e1f7c26c
Jason Rikard <jsnrkd@gmail.com>
2014-08-23 18:54:36 -0400

    Made a pause menu!

---
+++ barrel.spritebuilder/Source/PauseScene.h
@@ -0,0 +1,13 @@
+//
+//  PauseScene.h
+//  barrel
+//
+//  Created by Jason Rikard on 8/23/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface PauseScene : CCNode
+
+@end
---
+++ barrel.spritebuilder/Source/PauseScene.m
@@ -0,0 +1,13 @@
+//
+//  PauseScene.m
+//  barrel
+//
+//  Created by Jason Rikard on 8/23/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "PauseScene.h"
+
+@implementation PauseScene
+
+@end
